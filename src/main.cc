a60b29909501d0cca0f7bf0379a15f55d6660b6b
Andrey Erokhin <language.lawyer@gmail.com>
2024-11-27 22:37:04 +0500

    Support window resize
    Why not, if we can

--- src/main.cc
+++ src/main.cc
@@ -1,44 +1,68 @@
 #include <SFML/Graphics.hpp>
 
+#include <stdexcept>
 
-int main()
+
+sf::Image paint(unsigned width, unsigned height)
 {
-	constexpr int image_width = 256;
-	constexpr int image_height = 256;
+	sf::Image ret;
+	ret.create(width, height);
 
-	sf::Image image;
-	image.create(image_width, image_height);
-
-	for (int y = 0; y < image_height; y++)
+	for (unsigned y = 0; y < height; y++)
 	{
-		for (int x = 0; x < image_width; x++)
+		for (unsigned x = 0; x < width; x++)
 		{
-			sf::Uint8 r = 255.999 * x / (image_width - 1);
-			sf::Uint8 g = 255.999 * y / (image_height - 1);
+			sf::Uint8 r = 255.999 * x / (width - 1);
+			sf::Uint8 g = 255.999 * y / (height - 1);
 			sf::Uint8 b = 0;
 
-			image.setPixel(x, y, {r, g, b});
+			ret.setPixel(x, y, {r, g, b});
 		}
 	}
 
-	sf::Texture texture;
-	if (!texture.loadFromImage(image))
-		return -1;
+	return ret;
+}
 
-	sf::Sprite sprite(texture);
+int main()
+{
+	constexpr int window_w = 256;
+	constexpr int window_h = 256;
 
-	sf::RenderWindow window({image_width, image_height}, "");
+	sf::RenderWindow window({window_w, window_h}, "");
 	while (window.isOpen())
 	{
-		window.clear();
-		window.draw(sprite);
-		window.display();
+		bool need_redraw = false;
 
 		sf::Event event;
 		while (window.waitEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::Resized) {
+				auto [w, h] = event.size;
+				sf::FloatRect rect(0, 0, w, h);
+
+				window.setView(sf::View(rect));
+				need_redraw = true;
+
+				break;
+			}
+		}
+
+		if (need_redraw)
+		{
+			auto [width, height] = window.getSize();
+
+			sf::Texture texture;
+			if (!texture.loadFromImage(paint(width, height)))
+				throw std::runtime_error("texture.loadFromImage");
+			sf::Sprite sprite(texture);
+
+			window.draw(sprite);
+			window.display();
+
+			need_redraw = false;
 		}
 	}
 }
