df79d24ff976e2598999f984a15c875380fa7074
Andrey Erokhin <language.lawyer@gmail.com>
2024-12-07 20:05:43 +0500

    Render a simple world with 2 spheres and sky
    In the window's event loop, do one round of path tracing over the entire buffer, average with previously traced buffers, and present the updated image
    The current number of traced buffers is shown in the window title
    
    Still no anti-aliasing

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include "camera.hpp"
+#include "hittable.hpp"
 #include "ray.hpp"
 #include "types.hpp"
 
@@ -7,83 +8,205 @@
 #include <glm/geometric.hpp>
 #include <glm/gtx/intersect.hpp>
 
+#include <cmath>
+#include <cstddef>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <numeric>
+#include <random>
 #include <stdexcept>
+#include <string>
+#include <utility>
 #include <vector>
 
 using namespace rt;
 
+std::random_device rd;
+std::mt19937 engine(rd());
 
-color ray_color(const ray& r)
+class sky : public hittable
 {
-	const auto& dir = r.direction();
-
-	vec3 pos, nrm;
-	if (glm::intersectRaySphere(r.origin(), dir, point3(0, 0, 1), 0.5, pos, nrm))
-		return (nrm + 1_flt) / 2_flt;
-
-	auto a = (dir.y + 1) / 2;
-
-	return (1 - a) * color(1, 1, 1) + a * color(.5, .7, 1);
-}
-
-int main()
-{
-	const int window_w = 320;
-	const int window_h = 240;
-
-	std::vector<color> buf;
-	std::vector<sf::Uint8> rgba;
-	sf::Image image;
-
-	sf::RenderWindow window({window_w, window_h}, "");
-	while (window.isOpen())
+	bool hit(const ray& r, hit_record& rec) override
 	{
-		bool need_redraw = false;
+		rec.t = std::numeric_limits<flt>::max();
+		rec.next.reset();
 
-		sf::Event event;
-		while (window.waitEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+		auto a = (r.direction().y + 1) / 2;
+		rec.material = [=](color& c) {
+			c = (1 - a) * color(1, 1, 1) + a * color(.5, .7, 1);
+		};
 
-			if (event.type == sf::Event::Resized) {
-				auto [w, h] = event.size;
-				sf::FloatRect rect(0, 0, w, h);
+		return true;
+	}
+};
 
-				window.setView(sf::View(rect));
-				need_redraw = true;
+class diffuse_sphere : public hittable
+{
+	const point3 center;
+	const flt radius;
 
+public:
+	diffuse_sphere(point3 center, flt radius)
+	: center(std::move(center))
+	, radius(radius)
+	{}
+
+	bool hit(const ray& r, hit_record& rec) override
+	{
+		flt dst;
+		auto hits = glm::intersectRaySphere(r.origin(), r.direction(), center, radius * radius, dst);
+
+		if (!hits) return false;
+
+		auto hitpoint = r.at(dst);
+		auto normal = (hitpoint - center) / radius;
+
+		// The following seems to fix shadow acne, but whatever,
+		// lets use a more generic fix (see below)
+		// Although, per-object fix taking into account its geometry
+		// can also make sense
+		// if (glm::dot(r.direction(), normal) >= 0) return false;
+
+		rec.t = dst;
+		rec.normal = normal;
+
+		std::uniform_real_distribution<flt> distr(-1, 1);
+		while (true) {
+			auto p = vec3(distr(engine), distr(engine), distr(engine));
+			auto len = glm::length(p);
+
+			if (len >= 16 * std::numeric_limits<flt>::min() and len <= 1) {
+				// could not do it with glm::faceforward
+				if (glm::dot(p, normal) < 0) p = -p;
+				// Lambertian reflection
+				rec.next.emplace(hitpoint, normal + p / std::sqrt(len));
 				break;
 			}
 		}
 
-		if (need_redraw)
+		rec.material = [](color& c) {
+			c *= 0.5;
+		};
+
+		return true;
+	}
+};
+
+class world
+{
+	const std::size_t maxHits;
+	std::vector<hit_record> hits;
+
+	std::vector<std::shared_ptr<hittable>> objects = {
+		std::make_shared<sky>(),
+		std::make_shared<diffuse_sphere>(point3(0, 0, 1), 0.5),
+		std::make_shared<diffuse_sphere>(point3(0, -100.5, 1), 100),
+	};
+
+public:
+	world(std::size_t maxHits) : maxHits(maxHits) { hits.reserve(maxHits); }
+
+	color ray_color(const ray& r)
+	{
+		hits.clear();
+
+		auto cur_ray = r;
+		for (std::size_t i = 0; i < maxHits; i++)
 		{
-			auto [width, height] = window.getSize();
+			hit_record current_hit = {
+				.t = std::numeric_limits<flt>::infinity(),
+				.material = [](color&) { /* does nothing */ },
+			};
+			hits.push_back(current_hit);
+			auto& closest_hit = hits.back();
 
-			buf.resize(width * height);
-			camera{}.render({ width, height, buf.data() }, ray_color);
-
-			rgba.resize(4 * width * height);
-			for (unsigned i = 0; i < width * height; i++)
+			for (const auto& o : objects)
 			{
-				const auto& color = buf.data()[i];
+				if (!o->hit(cur_ray, current_hit)) continue;
 
-				for (int j = 0; j < 3; j++) rgba[4 * i + j] = 255.999 * color[j];
-				rgba[4 * i + 3] = 255;
+				if (current_hit.t < closest_hit.t)
+					closest_hit = current_hit;
 			}
 
-			image.create(width, height, rgba.data());
+			if (!closest_hit.next) break;
 
-			sf::Texture texture;
-			if (!texture.loadFromImage(image))
-				throw std::runtime_error("texture.loadFromImage");
-			sf::Sprite sprite(texture);
-
-			window.draw(sprite);
-			window.display();
-
-			need_redraw = false;
+			const auto& next_ray= *closest_hit.next;
+			// moving away from the object is intended to fix the shadow acne
+			cur_ray = { next_ray.origin() + closest_hit.normal * 0.00001_flt, next_ray.direction() };
 		}
+
+		color ret(0, 0, 0);
+		for (auto hit = std::rbegin(hits); hit != std::rend(hits); ++hit)
+			hit->material(ret);
+
+		return ret;
+	}
+};
+
+int main()
+{
+	std::vector<color> buf, sum;
+	unsigned total = 0;
+
+	std::vector<sf::Uint8> rgba;
+	sf::Image image;
+
+	world w(16);
+
+	unsigned width = 480;
+	unsigned height = 360;
+	sf::RenderWindow window({width, height}, "");
+	while (window.isOpen())
+	{
+		sf::Event event;
+		if (!window.pollEvent(event))
+			goto no_events;
+
+		if (event.type == sf::Event::Closed)
+		{
+			window.close();
+			break;
+		}
+
+		if (event.type == sf::Event::Resized)
+		{
+			width = event.size.width;
+			height = event.size.height;
+
+			sf::FloatRect rect(0, 0, width, height);
+			window.setView(sf::View(rect));
+
+			buf.resize(width * height);
+			sum.clear();
+			sum.resize(width * height);
+			total = 0;
+			rgba.resize(4 * width * height);
+		}
+
+no_events:
+		camera{}.render({ width, height, buf.data() }, [&](const ray& r) {
+			return w.ray_color(r);
+		});
+		total++;
+
+		for (auto i = 0u; i < sum.size(); i++) sum[i] += buf[i];
+		for (auto i = 0u; i < sum.size(); i++)
+		{
+			auto c = sum[i] / flt(total);
+
+			for (int j = 0; j < 3; j++) rgba[4 * i + j] = 255.999 * c[j];
+			rgba[4 * i + 3] = 255;
+		}
+		image.create(width, height, rgba.data());
+
+		sf::Texture texture;
+		if (!texture.loadFromImage(image))
+			throw std::runtime_error("texture.loadFromImage");
+		sf::Sprite sprite(texture);
+
+		window.draw(sprite);
+		window.display();
+		window.setTitle(std::to_string(total));
 	}
 }
